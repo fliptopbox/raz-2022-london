ab25d15b56c69073423c41e6de783172438d7253
Bruce Thomas <burce.thomas@ably.com>
2022-01-30 22:46:58 +0000

    email form JS validation

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form[data-netlify]");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+    const form = document.querySelector("form");
+  const fullname = document.querySelector("[name=fullname]").value;
+  const emailaddress = document.querySelector("[name=emailaddress]").value;
+
+  const radios = document.querySelectorAll("[name=equipment]");
+  const confirmed = document.querySelector("[name=confirm]").checked;
+  const equipment = Array.from(radios).filter((el) => el.checked);
+
+  const table = equipment.length ? equipment[0].value : null;
+
+  let errors = [];
+  const remail = /^[^\s]+@[^\s]+$/i;
+  const rname = /^.{3,}$/i;
+
+  if (!rname.test(`${fullname}`.trim())) errors.push("Please enter fullname");
+  if (!emailaddress || !remail.test(emailaddress || ""))
+    errors.push("Illegal email address");
+
+  if (!table) errors.push("Please select equipment option.");
+  if (!confirmed) errors.push("Please confirm certification.");
+
+  console.log(fullname || "", emailaddress || "");
+  console.log(errors, errors.length);
+
+  if (errors.length) {
+    alert([...errors].join("\n"));
+    return false;
+  }
+
+    console.log(form);
+    form.innerHTML = `
+        <h3 style="margin-top:0">Thank you</h3>
+
+        <p>Your details have been sent successfully,
+        and you will recieve a confirmation shortly.</p>
+
+        <p>The moment we are ready to accept payments
+        an email will be sent to everyone so keep an eye open
+        because there are limited places for the workshop.</p>
+    `;
+  return true;
+};
